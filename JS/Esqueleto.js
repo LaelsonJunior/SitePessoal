5459ded13be71d1ff14ed91ee21d9f18ea590e8a
LyanKurama <Lyanrd@hotmail.com>
2022-11-29 14:50:34 -0300

    .

---
+++ JS/Esqueleto.js
@@ -0,0 +1,11 @@
+function controlaMenu()
+{
+    if (menu.style.display == 'none'){
+        menu.style.display = 'block'
+        menuHam.style.display = 'none'
+    }
+    else{
+        menu.style.display = 'none'
+        menuHam.style.display = 'block'
+    }
+}
